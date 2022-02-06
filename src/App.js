c599730a055ce1beba9e505a34b2fe49782082ef
JoynalAbedin <jolobangladesh@gmail.com>
2022-02-06 12:05:10 +0600

    breakfast design finished

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Banner from './components/Banner/Banner';
 import Header from './components/Header/Header';
+import Menu from './components/Menu/Menu';
 function App() {
   return (
     <div className="App">
      
      <Header></Header>
      <Banner></Banner>
+     <Menu></Menu>
     </div>
   );
 }
